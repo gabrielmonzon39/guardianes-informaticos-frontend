91b1924383dcdb40f5331e39f7e76c09ec936b11
Gabriel Monzon <gabriel.monzon39@gmail.com>
2022-08-17 01:46:43 -0600

    [Layouts] Main new file

---
+++ src/layouts/MainNew.vue
@@ -0,0 +1,18 @@
+<script>
+export default {};
+</script>
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Prueba Recorrido </q-toolbar-title>
+
+        <div>Aug 2022</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
