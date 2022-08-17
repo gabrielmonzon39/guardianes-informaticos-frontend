c44e9ec8c5155c8b3d26c899c58fc6e1c33c572f
Gabriel Monzon <gabriel.monzon39@gmail.com>
2022-08-17 16:51:39 -0600

    [Pages][Index] Added Edit button

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,70 +1,79 @@
 <template>
-  <div class="q-pa-md row">
-    <div class="col-4 q-pa-sm">
-      <q-select
-        v-model="client_selected"
-        :options="clients"
-        label="Seleccione un Cliente"
-        @update:model-value="getClientWeeks"
+  <div class="column">
+    <div class="q-pr-lg q-pt-lg col-12 text-right">
+      <q-btn
+        v-if="selected_week"
+        color="secondary"
+        label="Editar Disponibilidad"
+        :to="edit_route"
       />
-      <q-select
-        v-if="client_selected"
-        v-model="selected_week"
-        :options="weeks"
-        label="Seleccione una Semana"
-        :hint="from_to_week"
-        @update:model-value="getClientWeekSchedule"
-      />
-      <q-table
-        class="q-mt-md"
-        v-if="hours_by_worker"
-        title="Horas por Trabajador"
-        :columns="hours_by_worker_headers"
-        :rows="hours_by_worker"
-        :hide-bottom="true"
-        :hide-header="true"
-        row-key="name"
-      >
-        <template v-slot:body-cell-name="props">
-          <q-td :props="props">
-            <div>
-              <q-badge :color="props.row.color" :label="props.value" />
-            </div>
-          </q-td>
-        </template>
-      </q-table>
     </div>
-    <div class="col-4 q-pa-sm" v-for="day in schedule" :key="day.day_of_week">
-      <q-table
-        :title="day.date"
-        :rows="day.day_schedule"
-        row-key="time"
-        v-model:pagination="pagination"
-        :rows-per-page-options="[0]"
-        hide-header
-        hide-bottom
-      >
-        <template v-slot:body="props">
-          <q-tr :props="props">
-            <q-td key="time" :props="props" color="red">
-              <q-badge
-                :color="props.row.worker == 'Sin Asignar' ? 'red' : 'black'"
-                :label="props.row.time"
-              />
+    <div class="q-pa-md row">
+      <div class="col-4 q-pa-sm">
+        <q-select
+          v-model="client_selected"
+          :options="clients"
+          label="Seleccione un Cliente"
+          @update:model-value="getClientWeeks"
+        />
+        <q-select
+          v-if="client_selected"
+          v-model="selected_week"
+          :options="weeks"
+          label="Seleccione una Semana"
+          :hint="from_to_week"
+          @update:model-value="getClientWeekSchedule"
+        />
+        <q-table
+          class="q-mt-md"
+          v-if="hours_by_worker"
+          title="Horas por Trabajador"
+          :columns="hours_by_worker_headers"
+          :rows="hours_by_worker"
+          :hide-bottom="true"
+          :hide-header="true"
+          row-key="name"
+        >
+          <template v-slot:body-cell-name="props">
+            <q-td :props="props">
+              <div>
+                <q-badge :color="props.row.color" :label="props.value" />
+              </div>
             </q-td>
-            <q-td key="worker" :props="props" class="text-center">
-              <q-icon
-                v-if="props.row.worker == 'Sin Asignar'"
-                name="warning"
-                color="warning"
-              />
-              <q-badge v-else :color="worker_colors[props.row.worker]">{{
-                props.row.worker
-              }}</q-badge>
-            </q-td>
-          </q-tr>
-        </template>
-      </q-table>
+          </template>
+        </q-table>
+      </div>
+      <div class="col-4 q-pa-sm" v-for="day in schedule" :key="day.day_of_week">
+        <q-table
+          :title="day.date"
+          :rows="day.day_schedule"
+          row-key="time"
+          :rows-per-page-options="[0]"
+          hide-header
+          hide-bottom
+        >
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td key="time" :props="props" color="red">
+                <q-badge
+                  :color="props.row.worker == 'Sin Asignar' ? 'red' : 'green'"
+                  :label="props.row.time"
+                />
+              </q-td>
+              <q-td key="worker" :props="props" class="text-center">
+                <q-icon
+                  v-if="props.row.worker == 'Sin Asignar'"
+                  name="warning"
+                  color="warning"
+                />
+                <q-badge v-else :color="worker_colors[props.row.worker]">{{
+                  props.row.worker
+                }}</q-badge>
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+      </div>
     </div>
   </div>
 </template>
@@ -164,6 +173,12 @@ export default {
         return `del ${this.selected_week.from} al ${this.selected_week.to}`;
       }
       return "";
+    },
+    edit_route() {
+      if (this.client_selected && this.selected_week) {
+        return `/edit/${this.client_selected.value}/${this.selected_week.value}`;
+      }
+      return null;
     },
   },
 };
